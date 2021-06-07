e6ebcad64c11f41881627b99097f58b4ee17bb7c
eleidan-hunter <48258074+eleidan-hunter@users.noreply.github.com>
2021-06-07 21:40:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ app.use(express.static(__dirname + '/public'));
 
 const Prefix = '/api/v1';
 
+let RANDOM_NUMBER = 8;
+
 app.get(`${Prefix}/date`, (req, res) => {
     let date =  new Date();
     res.send(date.toLocaleString());
-})
+});
+
+app.get(`${Prefix}/try`, (req, res) => {
+
+    const { query } = req;
+
+    console.log(query.number);
+    res.send(`${RANDOM_NUMBER}`);
+});
 
 app.listen(3000);
 
--- public/main.js
+++ public/main.js
@@ -12,7 +12,28 @@ function getTime(){
     ).then(( result ) => {
         return result.text(); //json(), blob()
     }).then((data) => {
-        document.querySelector('body').innerText = data;
+        // document.querySelector('body').innerText = data;
     });
 }
 getTime();
+
+let Form = document.querySelector('form');
+let Input = document.querySelector('input');
+
+Form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let number = Input.value;
+    fetch(
+        `/api/v1/try?number=${number}`,
+        {
+            method: 'get',
+            headers: {},
+        }
+    ).then(( result ) => {
+        return result.text(); //json(), blob()
+    }).then((data) => {
+        console.log(data);
+    });
+})
+
